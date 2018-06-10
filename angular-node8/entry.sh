4938cd50cb8fd9d1b34947dcd437f059ecde4a22
Tahmim Ahmed shibli <shibli05@gmail.com>
2018-06-10 13:39:44 +0600

    angular development environment on docker.

---
+++ angular-node8/entry.sh
@@ -0,0 +1,10 @@
+echo "npm"
+npm -v
+echo "node"
+node -v
+echo "installing project dependencies."
+npm install
+echo "nd"
+ng version
+echo "angular app confiuration done."
+ng serve --host=0.0.0.0
